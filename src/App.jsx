08eaf3dedb811ef19da87ba1b66fa486ca50562d
Manuel Perez <manuelperez497078@gmail.com>
2023-04-06 16:29:16 +0000

    Se agrega countDown y Set Alert

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   const [count0000,setCount0000] = useState(0)
   const [count00000,setCount00000] = useState(0)
   const [count000000,setCount000000] = useState(0)
+  const [inputCD, setinputCD] = useState([])
 
 
   useEffect(() => {
@@ -70,6 +71,57 @@ function App() {
     setState(false)
   }
 
+  const setCountDown = (e) => {
+    setinputCD(e.target.value)
+
+  }
+
+  const onClickCD = () => {
+    if (inputCD.length < 7) {
+      for (let i = 0; i < inputCD.length; i++) {
+        const element = inputCD[i];
+        console.log(element)
+        if (inputCD.length === 1){
+          setCount(inputCD)
+        }
+        if (inputCD.length === 2){
+          setCount(inputCD[1])
+          setCount00(inputCD[0])
+        }
+        if (inputCD.length === 3){
+          setCount(inputCD[2])
+          setCount00(inputCD[1])
+          setCount000(inputCD[0])
+        }
+        if (inputCD.length === 4){
+          setCount(inputCD[3])
+          setCount00(inputCD[2])
+          setCount000(inputCD[1])
+          setCount0000(inputCD[0])
+        }
+        if (inputCD.length === 5){
+          setCount(inputCD[4])
+          setCount00(inputCD[3])
+          setCount000(inputCD[2])
+          setCount0000(inputCD[1])
+          setCount00000(inputCD[0])
+        }
+        if (inputCD.length === 6){
+          setCount(inputCD[5])
+          setCount00(inputCD[4])
+          setCount000(inputCD[3])
+          setCount0000(inputCD[2])
+          setCount00000(inputCD[1])
+          setCount000000(inputCD[0])
+        }
+      }
+      
+    } else {
+      alert("Invalid Input")
+    }
+  }
+
+
 
   return (
     <>
@@ -86,6 +138,18 @@ function App() {
         <button onClick={handleStopBtn}>Stop</button>
         <button onClick={handleResumeBtn}>Resume</button>
         <button onClick={handleResetBtn}>Reset</button>
+      </div>
+      <div className='optionsContainer'>
+        <div>
+          <p>Start CountDown</p>
+          <input type='text' onChange={setCountDown} value={inputCD}></input>
+          <button onClick={onClickCD}>Set</button>
+        </div>
+        <div>
+          <p>Set Alert</p>
+          <input type='text'></input>
+          <button>Set</button>
+        </div>
       </div>
     </>
   )
